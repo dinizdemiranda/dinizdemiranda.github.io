7737eb8430c42c434ffa1310cc6a0cf0985a3dfc
dinizdemiranda <dinizcavalcanti@hotmail.com>
2023-07-31 16:01:25 +0200

    heheh

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// export default defineConfig({
-// 	plugins: [sveltekit()]
-// });
-
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [sveltekit()],
-	build: {
-	  outDir: './docs', // relative to index.html
-	  // emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
-	}
-  })
+	plugins: [sveltekit()]
+});
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+// 	plugins: [sveltekit()],
+// 	build: {
+// 	  outDir: './docs', // relative to index.html
+// 	  // emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
+// 	}
+//   })
 
